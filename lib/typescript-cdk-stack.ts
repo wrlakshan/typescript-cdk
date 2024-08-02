2d56d1d7cd53b34dffc2f07853ad7255e3d716dc
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-08-02 08:39:28 +0530

    Create S3 Bucket

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,16 +1,18 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "DocumentsBucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TypescriptCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "DocumentsBucketNameExports", {
+      value: bucket.bucketName,
+      exportName: "DocumentsBucketName",
+    });
   }
 }
